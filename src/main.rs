807fa3bd65f3d9b660ce5599e74ecb3b4913345d
alpaylan <alpkeles99@gmail.com>
2020-11-02 14:54:37 +0300

    City grid for default city type is written.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,62 @@
 use std::vec::Vec;
 use rand;
+use std::ops::{
+    Add,
+    Sub,
+};
 
-#[derive(Clone, Copy)]
-#[derive(Debug)]
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
 enum OneWayRoadType { Up, Left, Bottom, Right }
-#[derive(Clone, Copy)]
-#[derive(Debug)]
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
 enum TwoWayRoadType { UpLeft, UpRight, DownLeft, DownRight }
-#[derive(Clone, Copy)]
-#[derive(Debug)]
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
 enum RoadType {
     OneWay(OneWayRoadType),
     TwoWay(TwoWayRoadType)
 }
-#[derive(Clone, Copy)]
-#[derive(Debug)]
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
 enum ParkType { Free, Full }
-#[derive(Clone, Copy)]
-#[derive(Debug)]
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
 enum LightCond { Green, Red }
-#[derive(Debug)]
-#[derive(Clone, Copy)]
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
 enum Tile {
     Building,
     Park(ParkType),
     Light(LightCond),
     Road(RoadType),
+    Unknown,
 }
-#[derive(Clone, Copy)]
-#[derive(Debug)]
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
 struct Position { x: u32, y: u32 }
 type Direction = Position;
+
+impl Add<Position> for Position {
+    type Output = Position;
+    fn add(self, rhs: Position) -> Self::Output {
+        return Position {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y
+        }
+    }
+}
+
+impl Sub<Position> for Position {
+    type Output = Position;
+    fn sub(self, rhs: Position) -> Self::Output {
+        return Position {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y
+        }
+    }
+}
+
+
 #[derive(Debug)]
 struct Layout {
     b_num_x: u32,
@@ -53,6 +79,113 @@ struct City {
     buildings: Vec<TargetType>,
 }
 impl City {
+    fn calculate_tile_type(city_type: CityType, b_size: u32, i: u32, j: u32) -> Tile {
+        let bottom = RoadType::OneWay(OneWayRoadType::Bottom);
+        let up = RoadType::OneWay(OneWayRoadType::Up);
+        let left = RoadType::OneWay(OneWayRoadType::Left);
+        let right = RoadType::OneWay(OneWayRoadType::Right);
+        let tile_type: Tile = match city_type {
+            CityType::Default => {
+                let unit = (b_size+4) as i64;
+                let unit_y = i as i64 % unit as i64 - b_size as i64;
+                let unit_x = j as i64 % unit as i64 - b_size as i64;
+                let tile_type_x : Tile = match unit_x {
+                    0 | 3 => { Tile::Park(ParkType::Free) },
+                    1 => { Tile::Road(bottom) },
+                    2 => { Tile::Road(up) },
+                    _ => { Tile::Building }
+                };
+                let tile_type_y : Tile = match unit_y {
+                    0 | 3 => { Tile::Park(ParkType::Free) },
+                    1 => { Tile::Road(left) },
+                    2 => { Tile::Road(right) },
+                    _ => { Tile::Building }
+                };
+                let tile =
+                    match tile_type_x {
+                        Tile::Park(..) => {
+                            match tile_type_y {
+                                Tile::Park(..) => {
+                                    Tile::Light(LightCond::Green)
+                                },
+                                Tile::Building => {
+                                    Tile::Park(ParkType::Free)
+                                },
+                                Tile::Road(x) => {
+                                    Tile::Road(x)
+                                }
+                                _ => {
+                                    Tile::Unknown
+                                }
+                            }
+                        },
+                        Tile::Building => {
+                            match tile_type_y {
+                                Tile::Building => {
+                                    Tile::Building
+                                },
+                                Tile::Park(..) => {
+                                    Tile::Park(ParkType::Free)
+                                },
+                                Tile::Road(x) => {
+                                    Tile::Road(x)
+                                },
+                                _ => {
+                                    Tile::Unknown
+                                }
+                            }
+                        },
+                        Tile::Road(x) => {
+                            match tile_type_y {
+                                Tile::Building | Tile::Park(..)=> {
+                                    Tile::Road(x)
+                                },
+                                Tile::Road(y) => {
+                                    match (y, x) {
+                                        (RoadType::OneWay(OneWayRoadType::Right),
+                                        RoadType::OneWay(OneWayRoadType::Up))  => {
+                                            Tile::Road(RoadType::TwoWay(TwoWayRoadType::UpRight))
+                                        },
+                                        (RoadType::OneWay(OneWayRoadType::Left),
+                                            RoadType::OneWay(OneWayRoadType::Up))  => {
+                                            Tile::Road(RoadType::TwoWay(TwoWayRoadType::UpLeft))
+                                        },
+                                        (RoadType::OneWay(OneWayRoadType::Right),
+                                            RoadType::OneWay(OneWayRoadType::Bottom))  => {
+                                            Tile::Road(RoadType::TwoWay(TwoWayRoadType::DownRight))
+                                        },
+                                        (RoadType::OneWay(OneWayRoadType::Left),
+                                            RoadType::OneWay(OneWayRoadType::Bottom))  => {
+                                            Tile::Road(RoadType::TwoWay(TwoWayRoadType::DownLeft))
+                                        },
+                                        _ => {
+                                            Tile::Unknown
+                                        }
+                                    }
+                                },
+                                _ => {
+                                Tile::Unknown
+                            }
+                            }
+                        }
+                        _ => {
+                            Tile::Unknown
+                        }
+                    };
+                return tile
+            },
+            CityType::Bordered => {
+                Tile::Unknown
+            },
+            CityType::Line => {
+                Tile::Unknown
+            },
+            _ => {
+                Tile::Unknown
+            }
+        };
+        return tile_type;
+    }
     fn generate_entry_points(city_type: CityType, b_num_x: u32, b_num_y: u32, b_size: u32) -> Vec<Position> {
         let mut entry_point_vec: Vec<Position> = Vec::new();
         match city_type {
@@ -123,12 +256,12 @@ impl City {
         let len_x = City::city_length_x(city_type, b_num_x, b_size);
         let len_y = City::city_length_y(city_type, b_num_y, b_size);
         let mut tiles : Vec<Vec<Tile>> = Vec::with_capacity(len_x as usize);
-        for i in 0..len_x {
+        for _ in 0..len_x {
             tiles.push(vec![Tile::Building; len_y as usize]);
         }
         for i in 0..len_x {
             for j in 0..len_y {
-                tiles[i as usize][j as usize] = Tile::Building;
+                tiles[i as usize][j as usize] = City::calculate_tile_type(city_type, b_size, i, j);
             }
         }
         return tiles
@@ -199,7 +332,7 @@ impl Simulation {
         let r = rand::random::<usize>()%l;
         return city.buildings[r];
     }
-    pub fn generate_cars(city: &City, n_cars: u32) -> Vec<Car>{
+    pub fn generate_cars(city: &City, n_cars: u32) -> Vec<Car> {
             let mut c_vec = Vec::with_capacity(n_cars as usize);
             for i in 0..n_cars {
                 c_vec.push(Car {
